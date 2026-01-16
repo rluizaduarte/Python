5d71e3897216cef9e1fca6bc435c51bf94473a77
rluizaduarte <riellyduarte75@gmail.com>
2026-01-16 15:50:08 -0300

    Aula 1

---
+++ Introducao/coments.py
@@ -0,0 +1,9 @@
+# comentários sao ignorados pelo interpretador
+print("Hello World!") # aqui o interpretador só enxerga a função print
+"""docstring
+são usados como coments
+não são ignorados pelo interpretador
+são lidos
+é multilinha
+precisa usar o mesmo tipo de aspas p abrir e p fechar
+"""
