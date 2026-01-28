94cdcc1fa0c9d9178fb0c265399ee22ec503d7ae
rluizaduarte <riellyduarte75@gmail.com>
2026-01-28 18:15:24 -0300

    Tratamento de erros específicos

--- Introducao/erros.py
+++ Introducao/erros.py
@@ -3,6 +3,7 @@ serve para tratar erros que podem acontecer
 tenta executar o bloco try
 se um erro acontecer, o bloco except é executado
 """
+
 entrada = input("digite um numero para ser dobrado: ")
 try:
     print(f"você digitou {entrada}")
@@ -12,3 +13,16 @@ try:
 except:
     print(f"{entrada} não é um número")
 
+"""try/except especifico
+é possivel tratar erros específicos
+p isso, precisa saber o nome do erro
+"""
+
+entrada = input("digite um numero para ser dobrado: ")
+try:
+    print(f"você digitou {entrada}")
+    numero_float = float(entrada) 
+    print(f"o dobro de {numero_float} é {numero_float * 2}")
+except ValueError:
+    # trata apenas erros do tipo ValueError q é quando n consegue converter p numero
+    print(f"{entrada} não é um número válido")
