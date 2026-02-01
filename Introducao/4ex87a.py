d18534fedfd1411105121d748640c1f2a0a868fb
rluizaduarte <riellyduarte75@gmail.com>
2026-02-01 19:08:05 -0300

    Índices e itens da lista

---
+++ Introducao/4ex87a.py
@@ -0,0 +1,10 @@
+"""
+Exiba os índices da lista
+"""
+
+# cont = 0
+lista = ["maçã", "banana", "laranja", "uva", "pera"]
+for item in lista:
+    print(lista.index(item), item)
+    # print(cont)
+    # cont += 1
