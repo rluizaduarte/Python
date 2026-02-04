fdd29e80b5b26a41b0dac7fb318af643673b7c43
rluizaduarte <riellyduarte75@gmail.com>
2026-02-04 17:27:19 -0300

    Comandos do interpretador do python e regras de convivência

---
+++ Introducao/interpretador.py
@@ -0,0 +1,34 @@
+"""interpretador do python
+
+alguns comandos especificos e uteis
+  python --version mostra a versão do python q vc ta 
+  python nome_do_arquivo.py roda o arquivo python
+  python -u nome_do_arquivo.py roda o arquivo python sem bufferizar a saída
+    bufferizar é qnd o pc guarda a saída na memória antes de mostrar na tela
+  python -m nome_do_modulo roda um módulo como script
+    é qnd vc quer rodar um módulo q n é um arquivo .py direto
+  python -c "comando" roda um comando python direto no terminal
+  python -i nome_do_arquivo.py roda o arquivo python e depois entra no modo interativo
+    esse modo é bom pra debugar e testar a scoisas 
+
+the zen of python, por tim peters
+  bonito é melhor q feio
+  explícito é melhor q implícito
+  simples é melhor q complexo
+  complexo é melhor q complicado
+  plano é melhor q aglomerado
+  esparso é melhor q denso
+  legibilidade conta
+  casos especiais n sao especiais o bastante p quebrar as regras
+  embora a praticidade vença a pureza
+  erros nunca devem passar silenciosamente
+  a menos q sejam explicitamente silenciados
+  diante da ambiguidade, recuse a tentação de adivinhar
+  deve haver um -- e so um -- modo óbvio de fazer algo
+  embora esse modo possa n ser óbvio a princípio, a menos q vc seja holandês
+  agr é melhor q nunca
+  embora nunca seja, muitas vezes, melhor q já
+  se a implementação é difícil de explicar, é uma má ideia
+  se a implementação é fácil de explicar, pode ser uma boa ideia
+  namespace são uma ótima ideia -- vamos fazer mais dessas!
+"""
