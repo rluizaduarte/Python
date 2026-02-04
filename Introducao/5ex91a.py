5d3994c99c554a6095ec1de5cffe5c4d30ed17ce
rluizaduarte <riellyduarte75@gmail.com>
2026-02-04 15:33:34 -0300

    Lista de compras (correção)

--- Introducao/5ex91a.py
+++ Introducao/5ex91a.py
@@ -12,20 +12,25 @@ lista = []
 while True:
     print("selecione uma opção")
     opcao = input("[I]nserir [A]pagar [L]istar: ").lower()
+    # a cadeia de if poderia ser mais simples
+    # se é i, se é a, se é l ou se n é nenhum desses
     if opcao not in "ial":
         os.system("cls")
         print("você não selecionou uma opção válida")
         continue
+        # n precisa desse continue
     else:
         if len(lista) < 1 and opcao in "al":
             if opcao == 'a':
                 os.system("cls")
                 print("n tem nada p apagar na sua lista")
                 continue
+                # n precisa desse tb
             elif opcao == 'l':
                 os.system("cls")
                 print("sua lista ainda ta vazia. tente inserir algo")
                 continue
+                # nem desse
         else:
             if opcao == 'i':
                 item = input("nome do item: ")
@@ -35,12 +40,16 @@ while True:
             elif opcao == 'a':
                 indice = int(input("indice do item para remover: "))
                 try:
+                    # poderia colocar o int(indice) aq
                     lista.pop(indice)
                     os.system("cls")
                     print("item removido da lista com sucesso")
+                # except index error
                 except:
                     os.system("cls")
                     print("o índice que vc informou é inválido")
+                # por fim ainda podia colocar except exception q pega qqr erro
+                # a saida seria "erro desconhecido" ou outra cs
             elif opcao == 'l':
                 os.system("cls")
                 lista_enumerada = enumerate(lista)
