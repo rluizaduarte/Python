5f5a79cb2590789be4b6cc5642afe92ef3fdbca3
rluizaduarte <riellyduarte75@gmail.com>
2026-02-13 23:55:15 -0300

    Exercício com funções (correção)

--- S2 - Python Intermediário/1ex114a.py
+++ S2 - Python Intermediário/1ex114a.py
@@ -5,6 +5,8 @@ Crie uma função que multiplca todos os argumentos não nomeados recebidos
 Retorne o total para uma variavel e imprima o resultado
 """
 
+# poderia tratar os valores
+
 def multiplica(*args):
     resultado = 1
     for numero in args:
@@ -26,6 +28,8 @@ def par_ou_impar(numero):
     if numero % 2 == 0:
         return f"{numero} é par"
     return f"{numero} é impar"
-
+    # poderia retornar só true ou false
+    # return numero % 2 == 0
+    
 print(par_ou_impar(10))
 print(par_ou_impar(11))
