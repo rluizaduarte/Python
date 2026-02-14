2ceb4520b8e70fac700cbdb472fbdaf1ef6c5bbf
rluizaduarte <riellyduarte75@gmail.com>
2026-02-13 23:51:00 -0300

    Exercício com funções (corrigir)

---
+++ S2 - Python Intermediário/1ex114a.py
@@ -0,0 +1,31 @@
+# Exercicios com funcoes
+
+"""
+Crie uma função que multiplca todos os argumentos não nomeados recebidos
+Retorne o total para uma variavel e imprima o resultado
+"""
+
+def multiplica(*args):
+    resultado = 1
+    for numero in args:
+        resultado *= numero
+    return resultado
+
+total = multiplica(2, 3, 4)
+print(total)
+tupla_total = (9, 27, 44)
+novo_total = multiplica(*tupla_total)
+print(novo_total)
+
+"""
+Crie uma função que diz se um numero é par ou impar
+Retorne o resultado para uma variavel e imprima o resultado
+"""
+
+def par_ou_impar(numero):
+    if numero % 2 == 0:
+        return f"{numero} é par"
+    return f"{numero} é impar"
+
+print(par_ou_impar(10))
+print(par_ou_impar(11))
