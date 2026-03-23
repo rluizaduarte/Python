e927f27d9eccdde51b8a473e2cd2695ec2d80268
rluizaduarte <riellyduarte75@gmail.com>
2026-03-22 21:29:30 -0300

    Mais métodos!

--- S2 - Python Intermediário/dicionarios.py
+++ S2 - Python Intermediário/dicionarios.py
@@ -89,3 +89,28 @@ print(pessoa_copia)
 # atribuição: aponta p mesmo canto. se eu alterar um, o original altera
 # shallow copy: cria um novo, mas os itens mutaveis ainda apontam p mesma ref. mudar a lista em um altera no outro
 # deep copy: cria um novo e os itens mutaveis tb sao copiados. muda em um n muda no outro 
+
+pessoa.pop("sobrenome")
+# apaga um item c a chave especificada 
+# mas ele ainda retorna o valor ent eu posso atribuir a uma var
+idade = pessoa.pop("idade")
+print(idade)
+print(pessoa)
+
+ultimo_item = pessoa.popitem()
+# apaga o ultimo item adicionado e tb retorna um par chave-valor como tupla
+print(ultimo_item)
+print(pessoa)
+
+pessoa.update({
+    "sobrenome" : "mendonca", 
+    "peso" : 60
+    })
+# atualiza o dicionario com itens novos ou modificando itens antigos
+# esse metodo tb pode receber qqr iteravel como valor tipo lista ou tupla
+tupla = (("sobrenome", "outro"), ("peso", 10))
+pessoa.update(tupla)
+print(pessoa)
+lista = [["sobrenome", "qualquer outro"], ["peso", 20]]
+pessoa.update(lista)
+print(pessoa)
